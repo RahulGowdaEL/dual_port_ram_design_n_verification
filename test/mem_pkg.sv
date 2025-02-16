e394890e1887f15f8e923dd24ebd1a2db278c7cc
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-02-16 10:54:32 +0530

    Create mem_pkg.sv

---
+++ test/mem_pkg.sv
@@ -0,0 +1,14 @@
+package mem_package;
+    import uvm_pkg::*;
+   `include "uvm_macros.svh"
+   `include "mem_seqr.sv"
+   `include "mem_driver.sv"
+   `include "mem_monitor.sv"
+   `include "mem_seq_item.sv"
+   `include "mem_sequence.sv"
+   `include "mem_agent.sv"
+   `include "mem_wr_rd_test.sv"
+   `include "mem_env.sv"
+   `include "mem_soreboard"
+   `include "tb_top.sv"
+endpackage
